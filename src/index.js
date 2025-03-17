11db190a32069d054541893be3f021850f01c7c1
rafinha-g <rafaalbachgoldman@gmail.com>
2025-03-17 17:45:38 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "tf940677c30558f1o1b34a5a883a44b6";
   let context =
-    "You are a creative Poem expert and love to write short poems. You mission is to generate a 4 lines poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'Your favorite AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a creative Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'Your favorite AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
